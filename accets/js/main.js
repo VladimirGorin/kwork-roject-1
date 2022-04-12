e200e015bb47a1ff0b06ce61b6a93df2bc260b4b
Владимир Горин <vladimirgorin410@gmail.com>
2022-04-12 16:47:48 +0300

    first commit

---
+++ accets/js/main.js
@@ -0,0 +1,50 @@
+
+function addUser(){
+    document.getElementById('addPopup').style.display = 'flex'
+
+}
+
+function redirectUser(){
+    document.getElementById('redirectPopup').style.display = 'flex'
+
+}
+
+function addCar(){
+    document.getElementById('addCar').style.display = 'flex'
+
+}
+
+function redirectCar(){
+    document.getElementById('redirectCar').style.display = 'flex'
+
+}
+
+function closePopups(){
+
+    document.getElementById('redirectPopup').style.display = 'none'
+    document.getElementById('addPopup').style.display = 'none'
+
+}
+
+function closePopupsCars(){
+    document.getElementById('addCar').style.display = 'none'
+    document.getElementById('redirectCar').style.display = 'none'
+
+}
+
+function opennMenu(){
+    document.getElementById('menu').style.display = 'block'
+
+}
+
+
+function openMenu(){
+    document.getElementById('menu').style.display = 'block'
+
+}
+
+
+function closeMenu(){
+    document.getElementById('menu').style.display = 'none'
+
+}
